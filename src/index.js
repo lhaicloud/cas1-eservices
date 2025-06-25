ec38a828835a60ff73b12d3a7338b3637a1e8c54
lhaicloud <46161368+lhaicloud@users.noreply.github.com>
2025-06-25 13:35:25 +0800

    fix on marker icon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import store from "./store";
 import 'sweetalert2/dist/sweetalert2.min.css';
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix Leaflet's default icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 import './style.css'
 import './serviceWorker';
